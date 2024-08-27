c37b8693c643e7a96716e942fabcff32efef1f0d
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-08-27 22:29:59 +0200

    Add signIn and session user function

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,14 +10,30 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+
+    session.user.id = sessionUser._id.toString();
+
+    return session;
+  },
   async signIn({ profile }) {
     try {
       await connectToDb();
 
       // Check if a user already exist
+      const userExists = await User.findOne({ email: profile.email });
 
       // If not, create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
 
       return true;
     } catch (error) {
